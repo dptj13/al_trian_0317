9c3940f3456557abfddf4541a2a9e7588d638c5e
mtname <wseo4437@naver.com>
2025-03-18 10:55:36 +0900

    1

--- setting.js
+++ setting.js
@@ -1,7 +1,7 @@
-let myname="홍길동"
-let myproject="스프링 WAS 활용한 로그인 시스템"
+let myname = "윤예서";
+let myproject = "케라스기반 텐서플로우 AI 모델  포토폴리오";
 
-$(()=>{
-	$("#maintitle").text(`${myproject} [${myname}]`)
-	$("title").text(`${myproject} [${myname}]`)
-})
+$(() => {
+  $("#maintitle").text(`${myproject} [${myname}]`);
+  $("title").text(`${myproject} [${myname}]`);
+});
